b67100dfd660bf788e5f6a44c50d483f154e3607
Yuanzhe Dong <yuanzhedong@gmail.com>
2022-06-17 22:58:37 -0700

    Add plotter

--- Assignment_1/Assignment 1/src/Plotter.cpp
+++ Assignment_1/Assignment 1/src/Plotter.cpp
@@ -1,11 +1,48 @@
 #include "Plotter.h"
+#include "strlib.h"
 #include <iostream>
 using namespace std;
 
 void runPlotterScript(istream& input) {
+    float current_x = 0, current_y = 0;
+    bool pen_down = false;
+    auto style = PenStyle{ 4, "black" };
     for(string line; getline(input, line); ) {
         cout << line << endl;
-    }
+        Vector<string> tokens = stringSplit(line, " ");
+        if (toLowerCase(tokens[0]) == "moveabs") {
+            float target_x = stringToReal(tokens[1]);
+            float target_y = stringToReal(tokens[2]);
+            if (pen_down) {
+                drawLine(current_x, current_y, target_x, target_y, style);
+            }
+            current_x = target_x;
+            current_y = target_y;
+        }
+         if (toLowerCase(tokens[0]) == "pendown") {
+             pen_down = true;
+         }
+         if (toLowerCase(tokens[0]) == "penup") {
+             pen_down = false;
+         }
+         if (toLowerCase(tokens[0]) == "moverel") {
+             float delta_x = stringToReal(tokens[1]);
+             float delta_y = stringToReal(tokens[2]);
+             if (pen_down) {
+                 drawLine(current_x, current_y, current_x + delta_x, current_y + delta_y, style);
+             }
+             current_x += delta_x;
+             current_y += delta_y;
+         }
+         if (toLowerCase(tokens[0]) == "pencolor") {
+             auto col = tokens[1];
+             style.color = col;
+         }
+         if (toLowerCase(tokens[0]) == "penwidth") {
+             auto wid = stringToReal(tokens[1]);
+             style.width = wid;
+         }
+        }
     /*
     PenStyle style = { 4, "black" }; // Pen style is initialized
     drawLine(0.5, 0.5, 0.1, 0.2, style);
